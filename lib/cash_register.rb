9c9accedf606f9f6c445d04f1a044229c70231e5
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-17 18:35:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,7 +15,7 @@ class CashRegister
   
   def apply_discount
     @total = @total - @total*@discount
-    "After the discount, the total comes to #{@total}"
+    "After the discount, the total comes to $#{@total}."
   end
   
   def items
