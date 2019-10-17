aabb27fd9abdfd5cfa71f4087e7bf0ea40378d40
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-17 18:28:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,11 +5,12 @@ class CashRegister
   def initialize(discount = 0)
     @discount = discount
     @total = 0.0
+    @all = []
   end
   
-  def add_item(item_name, price)
+  def add_item(item_name, price, quantitiy = 1)
     @total += price
-    
+    @all << item_name
   end
   
   def apply_discount
